b467766bedcd337b8a2b5763482d00769878f9ef
alexLee <lizhenyu@storswift.com>
2023-02-07 09:11:41 +0000

    Update FRC721 contracts

---
+++ fevm/hardhat/deploy/deployMomentSwapFRC721.ts
@@ -0,0 +1,41 @@
+import hre from 'hardhat';
+
+import type { MomentSwapFRC721 } from '../typechain-types/contracts/MomentSwapFRC721';
+import type { MomentSwapFRC721__factory } from '../typechain-types/factories/contracts/MomentSwapFRC721__factory';
+
+async function main() {
+  console.log('MomentSwap721 deploying....');
+
+  const owner = new hre.ethers.Wallet(
+    process.env.WALLET_PRIVATE_KEY || 'undefined',
+    hre.ethers.provider
+  );
+  const momentSwapFRC721Factory: MomentSwapFRC721__factory = <
+    MomentSwapFRC721__factory
+  >await hre.ethers.getContractFactory('MomentSwapFRC721', owner);
+
+  const momentSwapFRC721: MomentSwapFRC721 = <MomentSwapFRC721>(
+    await momentSwapFRC721Factory.deploy()
+  );
+  await momentSwapFRC721.deployed();
+  console.log('momentSwapFRC721 deployed to ', momentSwapFRC721.address);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
+
+// import path from 'path';
+//Optional: Log to a file for reference
+// await hre.run('logToFile', {
+//   filePath: path.resolve(__dirname, 'log.txt'),
+//   data: {
+//     network: 'hyperspace',
+//     chainId: momentSwapFRC721.deployTransaction.chainId,
+//     owner: momentSwapFRC721.deployTransaction.from,
+//     address: momentSwapFRC721.address,
+//     tx: momentSwapFRC721.deployTransaction.hash,
+//     explorerUrl: `https://hyperspace.filscan.io/address/general?address=${momentSwapFRC721.address}`,
+//   },
+// });
---
+++ fevm/hardhat/tasks/index.ts
@@ -0,0 +1,4 @@
+import './deploy';
+import './utils';
+import './mintMomentSwapNFT'
+import './getMomentSwapNFT'
