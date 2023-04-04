cb1c57ac2e08eac6b664006cb57301c91d28c9c1
tian zhou <zhoutian@storswift.com>
2023-04-04 14:57:21 +0800

    Add comments

--- fevm/hardhat/contracts/Account.sol
+++ fevm/hardhat/contracts/Account.sol
@@ -4,15 +4,26 @@ pragma solidity 0.8.19;
 import {IAccount, AccountData} from "./interfaces/IAccount.sol";
 import {IMoment} from "./interfaces/IMoment.sol";
 
+/// @notice This contract implements the IAccount interface and provides functionality for managing accounts.
 contract Account is IAccount {
+    /// @notice Total number of created accounts
     uint64 public totalAccountCount;
+
+    /// @notice Mapping of addresses to account IDs.
     mapping(address => uint64) public accountIds;
+
+    /// @notice Mapping of account IDs to account data.
     mapping(uint64 => AccountData) public accounts;
 
+    /// @notice Error to be thrown when an account already exists for the given address.
     error AccountAlreadyExists();
 
     constructor() {}
 
+    /// @notice Returns the account IDs of the given addresses.
+    /// @dev If an address does not have an account, it is omitted from the result.
+    /// @param addresses The list of addresses for which to retrieve the account IDs.
+    /// @return An array of account IDs corresponding to the given addresses.
     function getAccountIds(address[] calldata addresses) external view returns (uint64[] memory) {
         uint64[] memory _accountIds = new uint64[](addresses.length);
         for (uint256 i = 0; i < addresses.length; i++) {
@@ -21,6 +32,10 @@ contract Account is IAccount {
         return _accountIds;
     }
 
+    /// @notice Returns the addresses corresponding to the given account IDs.
+    /// @dev If an account ID does not exist, it is omitted from the result.
+    /// @param _accountIds The list of account IDs for which to retrieve the addresses.
+    /// @return An array of addresses corresponding to the given account IDs.
     function getAddresses(uint64[] calldata _accountIds) external view returns (address[] memory) {
         address[] memory addresses = new address[](_accountIds.length);
         for (uint256 i = 0; i < _accountIds.length; i++) {
@@ -29,6 +44,10 @@ contract Account is IAccount {
         return addresses;
     }
 
+    /// @notice Returns the account data for the given account IDs.
+    /// @dev If an account ID does not exist, it is omitted from the result.
+    /// @param _accountIds The list of account IDs for which to retrieve the account data.
+    /// @return An array of account data corresponding to the given account IDs.
     function getAccountData(uint64[] calldata _accountIds) external view returns (AccountData[] memory) {
         AccountData[] memory accountData = new AccountData[](_accountIds.length);
         for (uint256 i = 0; i < _accountIds.length; i++) {
@@ -37,6 +56,10 @@ contract Account is IAccount {
         return accountData;
     }
 
+    /// @notice Returns the avatar URIs for the given account IDs.
+    /// @dev If an account ID does not exist, it is omitted from the result.
+    /// @param _accountIds The list of account IDs for which to retrieve the avatar URIs.
+    /// @return An array of avatar URIs corresponding to the given account IDs.
     function getAvatarURIs(uint64[] calldata _accountIds) external view returns (string[] memory) {
         string[] memory avatarURIs = new string[](_accountIds.length);
         for (uint256 i = 0; i < _accountIds.length; i++) {
@@ -45,36 +68,101 @@ contract Account is IAccount {
         return avatarURIs;
     }
 
+    /// @notice Returns the moment IDs associated with the given account ID.
+    /// @param accountId The ID of the account for which to retrieve the moment IDs.
+    /// @return An array of moment IDs associated with the given account ID.
     function getMomentIds(uint64 accountId) external view returns (uint120[] memory) {
         return accounts[accountId].momentIds;
     }
 
+    /// @notice Returns the comment IDs associated with the given account ID.
+    /// @param accountId The ID of the account for which to retrieve the comment IDs.
+    /// @return An array of comment IDs associated with the given account ID.
     function getCommentIds(uint64 accountId) external view returns (uint128[] memory) {
         return accounts[accountId].commentIds;
     }
 
+    /// @notice Returns the moment IDs that the account has liked.
+    /// @param accountId The ID of the account for which to retrieve the liked moment IDs.
+    /// @return An array of moment IDs that the account has liked.
     function getLikedMomentIds(uint64 accountId) external view returns (uint120[] memory) {
          return accounts[accountId].likedMomentIds;
     }
 
+    /// @notice Returns the IDs of the spaces minted by the given account ID.
+    /// @param accountId The ID of the account for which to retrieve the minted space IDs.
+    /// @return An array of space IDs minted by the given account ID.
     function getMintedSpaceIds(uint64 accountId) external view returns (uint64[] memory) {
          return accounts[accountId].mintedSpaceIds;
     }
 
+    /// @notice Returns the IDs of the spaces rented by the given account ID.
+    /// @param accountId The ID of the account for which to retrieve the rented space IDs.
+    /// @return An array of space IDs rented by the given account ID.
     function getRentedSpaceIds(uint64 accountId) external view returns (uint64[] memory) {
         return accounts[accountId].rentedSpaceIds;
     }
 
+    /// @notice Creates a new account with the given domain name and avatar URI.
+    /// @param domainName The domain name to associate with the account.
+    /// @param avatarURI The URI of the avatar to associate with the account.
+    /// @return The ID of the newly created account.
     function createAccount(string calldata domainName, string calldata avatarURI) external returns (uint64) {}
+
+    /// @notice Cancels the account associated with the given account ID.
+    /// @dev The account must not have any associated moments, comments, or spaces in order to be cancelled.
+    /// @param accountId The ID of the account to cancel.
     function cancellationAccount(uint64 accountId) external {}
+
+    /// @notice Updates the avatar URI associated with the calling account.
+    /// @param avatarURI The new avatar URI to associate with the calling account.
     function updateAvatarURI(string calldata avatarURI) external {}
+
+    /// @notice Creates a new moment with the given metadata URI.
+    /// @param metadataURI The URI of the metadata to associate with the moment.
+    /// @return The ID of the newly created moment.
     function createMoment(string calldata metadataURI) external returns (uint120) {}
+
+    /// @notice Removes the moment associated with the given moment ID.
+    /// @dev The calling account must be the owner of the moment in order to remove it.
+    /// @param momentId The ID of the moment to remove.
     function removeMoment(uint120 momentId) external {}
+
+    /// @notice Adds a like to the moment associated with the given moment ID from the calling account.
+    /// @dev The calling account must not have already liked the moment.
+    /// @param momentId The ID of the moment to like.
     function likeMoment(uint120 momentId) external {}
+
+    /// @notice Cancels the like from the calling account to the moment associated with the given moment ID.
+    /// @dev The calling account must have already liked the moment.
+    /// @param momentId The ID of the moment to cancel the like for.
     function cancelLikeMoment(uint120 momentId) external {}
+
+    /// @notice Creates a new comment on the moment associated with the given moment ID with the given comment text.
+    /// @param momentId The ID of the moment to create the comment on.
+    /// @param commentText The text of the comment to create.
+    /// @return The ID of the newly created comment.
     function createComment(uint120 momentId, string calldata commentText) external returns (uint128) {}
+
+    /// @notice Removes the comment associated with the given comment ID.
+    /// @dev The calling account must be the owner of the comment in order to remove it.
+    /// @param commentId The ID of the comment to remove.
     function removeComment(uint128 commentId) external {}
+
+    /// @notice Mints a new child space domain with the given domain name and expire time.
+    /// @dev The calling account must own the parent space in order to mint a child space domain.
+    /// @param parentSpaceId The ID of the parent space to mint the child space domain for.
+    /// @param domainName The domain name to associate with the child space domain.
+    /// @param expireSeconds The number of seconds until the child space domain expires.
+    /// @return The ID of the newly minted child space domain.
     function mintChaildSpaceDomain(uint64 parentSpaceId, string calldata domainName, uint64 expireSeconds) external returns (uint64) {}
+
+    /// @notice Returns the space with the given space ID.
+    /// @param spaceId The ID of the space to return.
     function returnSpace(uint64 spaceId) external {}
+
+    /// @notice Updates the domain name for the rented space with the given space ID.
+    /// @param spaceId The ID of the rented space for which to update the domain name.
+    /// @param domainName The new domain name to set.
     function updateRentedSpaceDomainName(uint64 spaceId, string calldata domainName) external {}
 }
--- fevm/hardhat/contracts/Moment.sol
+++ fevm/hardhat/contracts/Moment.sol
@@ -3,16 +3,31 @@ pragma solidity 0.8.19;
 
 import {IMoment, MomentData, CommentData} from "./interfaces/IMoment.sol";
 
+/// @notice This contract implements the IMoment interface and provides functionality for managing moments.
 contract Moment is IMoment {
+
+    /// @notice Total number of moments created.
     uint120 public totalMomentCount;
+
+    /// @notice Total number of comment created.
     uint128 public totalCommentCount;
+
+    /// @notice Mapping of moment IDs to MomentData struct.
     mapping(uint120 => MomentData) public moments;
+
+    /// @notice Mapping of comment IDs to CommentData struct.
     mapping(uint128 => CommentData) public comments;
+
+    /// @notice Mapping of moment IDs to addresses of users who liked a moment.
     mapping(uint120 => address[]) public likes;
+
+    /// @notice Mapping of moment IDs to IDs of comments made on a moment.
     mapping(uint120 => uint128[]) public commentsOnMoment;
 
     constructor() {}
 
+    /// @notice Returns an array of all moments that have been created.
+    /// @return An array of MomentData structs representing all moments created in the contract.
     function getAllMoments() external view returns (MomentData[] memory) {
         MomentData[] memory allMoments = new  MomentData[](totalMomentCount);
         for (uint120 i = 0; i < totalMomentCount; i++) {
@@ -21,6 +36,9 @@ contract Moment is IMoment {
         return allMoments;
     }
 
+    /// @notice Returns an array of MomentData for the specified moment IDs.
+    /// @param momentIds The list of moment IDs for which to retrieve the MomentData.
+    /// @return An array of MomentData corresponding to the given moment IDs.
     function getMomentData(uint120[] calldata momentIds) external view returns (MomentData[] memory) {
         MomentData[] memory momentData = new  MomentData[](momentIds.length);
         for (uint120 i = 0; i < momentIds.length; i++) {
@@ -29,6 +47,9 @@ contract Moment is IMoment {
         return momentData;
     }
 
+    /// @notice Returns an array of the number of likes for each moment in the input array.
+    /// @param momentIds The list of moment IDs for which to retrieve the number of likes.
+    /// @return An array of the number of likes corresponding to the given moment IDs.
     function getLikes(uint120[] calldata momentIds) external view returns (uint256[] memory) {
         uint256[] memory likeCounts = new uint256[](momentIds.length);
         for (uint120 i = 0; i < momentIds.length; i++) {
@@ -37,6 +58,9 @@ contract Moment is IMoment {
         return likeCounts;
     }
 
+    /// @notice Returns an array of CommentData for the specified moment IDs.
+    /// @param momentIds The list of moment IDs for which to retrieve the CommentData.
+    /// @return An array of CommentData corresponding to the given moment IDs.
     function getComments(uint120[] calldata momentIds) external view returns (CommentData[] memory) {
         CommentData[] memory commentData = new CommentData[](momentIds.length);
         for (uint120 i = 0; i < momentIds.length; i++) {
@@ -45,10 +69,36 @@ contract Moment is IMoment {
         return commentData;
     }
 
+    /// @notice Creates a new moment and returns its ID.
+    /// @param accountId The ID of the account creating the moment.
+    /// @param metadataURI The URI of the metadata associated with the moment.
+    /// @return The ID of the newly created moment.
     function createMoment(uint64 accountId, string calldata metadataURI) external returns (uint120) {}
+
+    /// @notice Removes a moment with the specified ID.
+    /// @param momentId The ID of the moment to be removed.
+    /// @param accountId The ID of the account removing the moment.
     function removeMoment(uint120 momentId, uint64 accountId) external {}
+
+    /// @notice Adds a like to the specified moment for the specified account.
+    /// @param momentId The ID of the moment to add the like to.
+    /// @param accountId The ID of the account adding the like.
     function addLike(uint120 momentId, uint64 accountId) external {}
+
+    /// @notice Removes a like from the specified moment for the specified account.
+    /// @param momentId The ID of the moment to remove the like from.
+    /// @param accountId The ID of the account removing the like.
     function removeLike(uint120 momentId, uint64 accountId) external {}
+
+    /// @notice Creates a new comment for the specified moment and returns its ID.
+    /// @param momentId The ID of the moment to add the comment to.
+    /// @param accountId The ID of the account creating the comment.
+    /// @param commentText The text of the comment.
+    /// @return The ID of the newly created comment.
     function createComment(uint120 momentId, uint64 accountId, string calldata commentText) external returns (uint128) {}
+
+    /// @notice Removes a comment with the specified ID for the specified moment.
+    /// @param momentId The ID of the moment to remove the comment from.
+    /// @param commentId The ID of the comment to be removed.
     function removeComment(uint120 momentId, uint128 commentId) external {}
 }
